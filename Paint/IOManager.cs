a5932252db5300dc630916c891955e4b83d077be
HaruKishima <18120256@student.hcmus.edu.vn>
2022-01-07 09:34:59 +0700

    save and load layer from binary file

---
+++ Paint/IOManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paint
+{
+    public class IOManager
+    {
+        private IOManager? _instance;
+        private IOManager()
+        {
+
+        }
+        public IOManager GetInstance()
+        {
+            if (_instance == null)
+                _instance = new IOManager();
+            return _instance;
+        }
+        public void SaveToBinaryFile<T>(List<T> list, string file)
+        {
+            using (var stream = File.OpenWrite(file))
+            {
+                var formater = new BinaryFormatter();
+                formater.Serialize(stream, list);
+            }
+        }
+        public List<T>? LoadFromBinaryFile<T>(string file)
+        {
+            var list = new List<T>();
+            using (var stream = File.OpenRead(file))
+            {
+                var formatter = new BinaryFormatter();
+                list = formatter.Deserialize(stream) as List<T>;
+            }
+            return list;
+        }
+    }
+}
